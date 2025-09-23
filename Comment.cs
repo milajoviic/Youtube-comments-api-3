0d907eb03f8ee52b6ad94b9529bc2461982bbdcf
Mila Jovic <mila.jovic@elfak.rs>
2025-09-23 17:38:10 +0200

    Treci projekat

---
+++ Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeCommentAPI
+{
+    internal class Comment
+    {
+        public string AuthorName { get; set; }
+        public string Text { get; set; }
+        public int LikeCount { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+        public int ReplyCount { get; set; }
+    }
+}
