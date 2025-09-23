7204ce32d5d52c6e6cf43b7ebeab21f7b814d7df
Mila Jovic <mila.jovic@elfak.rs>
2025-09-23 18:09:08 +0200

    organizacija projekta

---
+++ URLSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeCommentAPI
+{
+    internal class URLSettings
+    {
+        private readonly string apiKey = "AIzaSyAf6mjDCUSfhf7RvT0GfXxQuDGvfqboA30";
+        private readonly int PortNum = 5050;
+        private readonly string basicUrl = "https://www.googleapis.com/youtube/v3/commentThreads";
+
+        public string GetURLPrefix()
+        {
+            return $"http://localhost:{PortNum}/";
+        }
+
+        public string GetURL(string videoId)
+        {
+            return $"{basicUrl}?part=snippet&videoId={videoId}&key={apiKey}";
+        }
+    }
+}
